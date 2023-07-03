0c6f165d8fafc28a609525a3575be671f0a25375
Hanjin <csa0545@gamil.com>
2023-07-03 09:40:03 +0900

    feat checkpoint

---
+++ contracts/Swap.sol
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./TokenPriceOracle.sol";
+import "./SelfToken.sol";
+
+contract Swap {
+    using SafeMath for uint256;
+    address public feeRecipient;
+    uint public feePercentage;
+    uint256 private AsdPrice;
+    uint256 public EthPrice;
+    uint256 public UsdtPrice;
+    uint256 public ArbPrice;
+    uint256 public EthSwapPercent;
+    uint256 public UsdtSwapPercent;
+    uint256 public ArbSwapPercent;
+    uint256 public testValue;
+
+    TokenPriceOracle public priceOracle;
+
+    address public EthAddress = 0x62CAe0FA2da220f43a51F86Db2EDb36DcA9A5A08;
+    address public UsdtAddress = 0x0a023a3423D9b27A0BE48c768CCF2dD7877fEf5E;
+    address public ArbAddress = 0x2eE9BFB2D319B31A573EA15774B755715988E99D;
+
+    constructor(uint _feePercentage) {
+        feeRecipient = address(this);
+        feePercentage = _feePercentage;
+        AsdPrice = 100000000;
+        priceOracle = new TokenPriceOracle(EthAddress, UsdtAddress, ArbAddress);
+        // EthPrice = priceOracle.getEthPrice();
+        // UsdtPrice = priceOracle.getUsdtPrice();
+        // ArbPrice = priceOracle.getArbPrice();
+        // EthSwapPercent = EthPrice / AsdPrice;
+        // UsdtSwapPercent = UsdtPrice / AsdPrice;
+        // ArbSwapPercent = ArbPrice / AsdPrice;
+        EthSwapPercent = 1878;
+        UsdtSwapPercent = 1;
+        ArbSwapPercent = 1;
+    }
+
+    function differTokenSwap(
+        address tokenA,
+        address tokenB,
+        address _userAccount,
+        address _contractAddress,
+        uint256 amountA
+    ) public {
+        uint256 amountB;
+        uint256 feeAmount;
+        uint256 amountToSwap;
+        uint256 swapPercent;
+        if (
+            keccak256(bytes(SelfToken(tokenA).name())) ==
+            keccak256(bytes("ARB"))
+        ) {
+            swapPercent = ArbSwapPercent;
+        } else if (
+            keccak256(bytes(SelfToken(tokenA).name())) ==
+            keccak256(bytes("USDT"))
+        ) {
+            swapPercent = UsdtSwapPercent;
+        } else if (
+            keccak256(bytes(SelfToken(tokenA).name())) ==
+            keccak256(bytes("ETH"))
+        ) {
+            swapPercent = EthSwapPercent;
+        } else {
+            revert("Unsupported token");
+        }
+
+        (feeAmount, amountToSwap) = calculateFeesAndAmountToSwap(amountA);
+
+        amountB = calculateAmountB(amountToSwap, swapPercent);
+
+        conductTransfer(
+            tokenA,
+            tokenB,
+            _userAccount,
+            _contractAddress,
+            amountA,
+            amountB,
+            feeAmount,
+            amountToSwap
+        );
+    }
+
+    function calculateFeesAndAmountToSwap(
+        uint256 amountA
+    ) public view returns (uint256 feeAmount, uint256 amountToSwap) {
+        uint256 _feePercentage = feePercentage * 10 ** 17;
+        feeAmount = (amountA / 10 ** 18) * _feePercentage;
+        amountToSwap = amountA - feeAmount;
+        return (feeAmount, amountToSwap);
+    }
+
+    function calculateAmountB(
+        uint256 amountToSwap,
+        uint256 swapPercent
+    ) public pure returns (uint256) {
+        return amountToSwap * swapPercent;
+    }
+
+    function conductTransfer(
+        address tokenA,
+        address tokenB,
+        address _userAccount,
+        address _contractAddress,
+        uint256 amountA,
+        uint256 amountB,
+        uint256 feeAmount,
+        uint256 amountToSwap
+    ) public payable {
+        SelfToken(tokenA).approve(_contractAddress, amountA);
+        SelfToken(tokenB).approve(_contractAddress, amountB);
+        SelfToken(tokenA).transferFrom(
+            _userAccount,
+            _contractAddress,
+            feeAmount
+        );
+        SelfToken(tokenA).transferFrom(
+            _userAccount,
+            _contractAddress,
+            amountToSwap
+        );
+        SelfToken(tokenB).transferFrom(_contractAddress, _userAccount, amountB);
+    }
+}
---
+++ contracts/governance.sol
@@ -0,0 +1,156 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./timelock.sol";
+
+contract Governance {
+    address private owner;
+    address private govToken;
+    uint256 private proposalAmount;
+    address public goverAddress;
+    address public timelock;
+
+    struct Receipt {
+        bool vote;
+        bool agree;
+    }
+
+    struct Proposal {
+        address proposer;
+        uint startBlock;
+        uint endBlock;
+        bytes callData;
+        bool canceled;
+        bool executed;
+        uint256 amountVote;
+        mapping(address => Receipt) hasVotes;
+    }
+
+    address[] public participants;
+
+    mapping(uint => Proposal) public proposes;
+    mapping(uint => address[]) public votes;
+
+    constructor(address _owner) {
+        owner = _owner;
+        goverAddress = address(this);
+    }
+
+    function propose(address _proposer, bytes memory _callData) public {
+        require(
+            IERC20(govToken).balanceOf(_proposer) > 0,
+            "Governance : Do not have a vASD token."
+        );
+
+        uint startBlock = block.number;
+        uint endBlock = block.number + 17280;
+        Proposal storage newProposal = proposes[proposalAmount + 1];
+        newProposal.proposer = _proposer;
+        newProposal.startBlock = startBlock;
+        newProposal.endBlock = endBlock;
+        newProposal.callData = _callData;
+        newProposal.canceled = false;
+        newProposal.executed = false;
+        newProposal.amountVote = 0;
+        newProposal.hasVotes[_proposer] = Receipt(true, true);
+        proposalAmount += 1;
+
+        ERC20(govToken)._burn(_proposer, 1); // burn 시킬 govtoken의 가치에 대해서 ?
+    }
+
+    function voting(address _participant, uint _proposal, bool _agree) public {
+        require(
+            IERC20(govToken).balanceOf(_participant) > 0,
+            "Governance : Do not have a vASD token."
+        );
+        require(
+            proposes[_proposal].hasVotes[_participant].vote == false,
+            "Governance : It is a proposal that has already been voted on."
+        );
+        // require(proposes[_proposal].endBlock > block.number, "Governance : It's an overdue vote.");
+        proposes[_proposal].hasVotes[_participant] = Receipt(true, _agree);
+        votes[_proposal].push(_participant);
+        proposes[_proposal].amountVote +=
+            uint256(IERC20(govToken).balanceOf(_participant) * 10 ** 18) /
+            IERC20(govToken).totalSupply();
+    }
+
+    function timelockExecute(uint _proposal) public returns (bool) {
+        require(msg.sender == owner, "Governance : only owner");
+        // require(proposes[_proposal].endBlock < block.number, "Governance : It hasn't been three days."); //3일 > 17,280
+        if (proposes[_proposal].amountVote > 0.51 * 10 ** 18) {
+            proposes[_proposal].executed = true;
+            Timelock(timelock).queueTransaction(
+                proposes[_proposal].callData,
+                block.timestamp
+            );
+            return true;
+        } else {
+            proposes[_proposal].canceled = true;
+            return false;
+        }
+    }
+
+    function proposalExecute(uint _proposal) public returns (bool) {
+        require(msg.sender == owner, "Governance : only owner");
+        // require(proposes[_proposal].endBlock < block.number, "Governance : It hasn't been three days.");
+        require(
+            proposes[_proposal].executed,
+            "Governance : It's a vote that didn't pass."
+        );
+        require(
+            Timelock(timelock).executeTransaction(
+                proposes[_proposal].callData,
+                block.timestamp
+            ),
+            "Governance : Timelock is running."
+        );
+        require(
+            Timelock(timelock)
+                .getTransaction(proposes[_proposal].callData)
+                .status
+        );
+        //proposes.callData 실행시켜야함
+        return true;
+    }
+
+    function makeCallData() public pure returns (bytes memory) {
+        return abi.encode(msg.data);
+    }
+
+    function changeLevel(address token) public {}
+
+    function changeOwner(address _newOwner) private {
+        owner = _newOwner;
+    }
+
+    function setTokenAddress(address _token) public {
+        require(owner == msg.sender);
+        govToken = _token;
+    }
+
+    function setTimelockAddress(address _timelock) public {
+        require(owner == msg.sender);
+        timelock = _timelock;
+    }
+
+    function getProposal(
+        uint _idx
+    )
+        public
+        view
+        returns (address, uint, uint, bytes memory, bool, bool, uint)
+    {
+        Proposal storage proposal = proposes[_idx];
+        return (
+            proposal.proposer,
+            proposal.startBlock,
+            proposal.endBlock,
+            proposal.callData,
+            proposal.canceled,
+            proposal.executed,
+            proposal.amountVote
+        );
+    }
+}
